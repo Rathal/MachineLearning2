0a8d0bd4fa7a5abdde6d69ae4ae9f611a1b40faf
Daniel <daniel_guy_98@hotmail.co.uk>
2019-12-17 18:51:28 +0000

    Section 3 Init

--- 16607811-ML2.py
+++ 16607811-ML2.py
@@ -65,35 +65,170 @@ for col in data:
         mean = data[col].mean()
         std = data[col].std()
         z[col] = ((data[col] - mean)/std)
-print(z.describe())
+
+#Converting Normal to 1, Abnormal to 0
+z = z.replace({'Normal': 1, 'Abnormal': 0})
+#print(z.describe())
+# =============================================================================
+
+#import sklearn
+from sklearn.neural_network import MLPClassifier
+#from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
+#from sklearn.metrics import mean_squared_error
+#from math import sqrt
+#from sklearn.metrics import r2_score
+#from sklearn.metrics import classification_report,confusion_matrix
+
+
+#Creating the training and test data sets
+# =============================================================================
+#Defines the Status column as the output for the ML algorithms
+outputCol = ['Status']
+#Defines the rest of the columns as the predictors
+inputCols = list(set(list(z.columns))-set(outputCol))
+X = z[inputCols].values
+Y = z[outputCol].values
+
+#Splits the data 90:10 train:test.
+xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size=0.1)
 # =============================================================================
 
 
-##Normalisation
-#Zi = (Xi - Xbar) / Sd
+#ANN
+# =============================================================================
+def ANN_fit(x, y, epochs):
+    mlp = MLPClassifier(hidden_layer_sizes=(500, 500), activation='logistic', max_iter=epochs)
+    mlp.fit(xTrain, yTrain.ravel())
+    return mlp
+def ANN_predict(mlp, x):
+    prediction = mlp.predict(x)
+    return prediction
 
+
+epochs = [1, 25, 50, 100, 250, 500, 1000, 5000] #1, 25, 50, 100, 250, 500
+accuracyANN = []
+for e in epochs:
+    print("Epoch: " + str(e))
+    mlp = ANN_fit(xTrain, yTrain, e)
+#    predictTest = ANN_predict(mlp, xTest)
+    accuracyANN.append(mlp.score(xTest, yTest))
+
+plt.plot(epochs,accuracyANN)
+plt.title("ANN Accuracy")
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(labels=['Testing', 'Training '])
+plt.show()
+# =============================================================================
+
+#Random Forest
+# =============================================================================
+from sklearn.ensemble import RandomForestRegressor
+
+trees = [1, 10, 50, 100, 1000, 5000]
+leaf = [5, 50]
+accuracyTree = []
+for l in leaf:
+    for tree in trees:
+        print("Leaf {0}: Tree {1}".format(l, tree))
+        regressor = RandomForestRegressor(n_estimators=tree, min_samples_leaf=l)
+        regressor.fit(xTrain, yTrain.ravel())
+        accuracyTree.append(regressor.score(xTest, yTest.ravel()))
+
+#print(accuracyTree)
+half = len(accuracyTree)//2
+accLeaf5 = accuracyTree[:half]
+accLeaf50 = accuracyTree[half:]
+#print(accLeaf50)
+
+plt.plot(trees,accLeaf5)
+plt.plot(trees,accLeaf50)
+plt.xscale('log')
+plt.xlabel('Trees')
+plt.ylabel('Accuracy')
+plt.legend(labels=['Leaf = 5', 'Leaf = 50'])
+plt.show()
+# =============================================================================
+
+
+#predict_train = mlp.predict(xTrain)
+#predict_test = mlp.predict(xTest)
+
+#print(mlp.score(xTrain, yTrain))
+#
+#print("Train Data:\n")
+#print(confusion_matrix(yTrain,predict_train))
+#print(classification_report(yTrain,predict_train))
+#print("Test data:\n")
+#print(confusion_matrix(yTest,predict_test))
+#print(classification_report(yTest,predict_test))
+
+
+
+
+# weights =2*np.random.random((2,2)) -1
+# 
+# for epoch in range(200):
+#     layer0 = trainData
+#     layer1 = sigmoid(np.dot(layer0,weights[0]))
+#     layer2 = sigmoid(np.dot(layer1,weights[1]))
+#     
+#     layer
+# 
+# =============================================================================
+# =============================================================================
+# class NeuralNetwork:
+#     def __init__(self, x, y):
+#         self.input = x
+#         self.weights1 = np.random.rand(self.input.shape[1],500)
+#         self.weights2 = np.random.rand(500,2)
+#         self.y = y
+#         self.output = np.zeros(y.shape)
+#     def feedforward(self):
+#         self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+#         self.output = sigmoid(np.dot(self.layer1, self.weights2))
+#     def backpropagation(self):
+#         d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_der(self.output)))
+#         d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_der(self.output), self.weights2.T) * sigmoid_der(self.layer1)))
+#         self.weights1 += d_weights1
+#         self.weights2 += d_weights2
+# 
+# ANN = NeuralNetwork(trainData,trainLabel)
+# ANN.feedforward()
+# ANN.backpropagation()
+# print(ANN.output)
+# =============================================================================
 
 # =============================================================================
-# f, (ax_box, ax_hist) = plt.subplots(2, sharex=False, gridspec_kw={"height_ratios": (.5, .5)}) #.15 .85
-# sns.boxplot(x="Status", y="Vibration_sensor_1", data=data, ax=ax_box)
+# #Hyperparameters
+# epochs = 100
+# layers = 2
+# neurons = 250
+# lr = 0.05
 # 
-# dfStatusNormal = data.loc[data['Status'] == 'Normal']
-# dfStatusAbnormal = data.loc[data['Status'] == 'Abnormal']
+# def init_param(layers, neurons):
+#     param = [None] *layers
+#     for i in range(layers):
+#         param[i] = np.zeros(neurons)
+#     return param
 # 
-# sns.distplot(dfStatusNormal['Vibration_sensor_2'], ax=ax_hist)
-# sns.distplot(dfStatusAbnormal['Vibration_sensor_2'], ax=ax_hist)
+# #Define Sigmoid Functions
+# def sigmoid(x):
+#     return 1/1(np.exp(-x))
+# def sigmoid_der(x):
+#     return sigmoid(x)*(1-sigmoid(x))
 # 
 # 
+# weights = init_param(layers, neurons)
+# bias = init_param(layers,neurons)
 # 
-# 
-# Attempting to Normalise Data
-# 
-# #Get numerical only data
-# numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
-# newData = data.select_dtypes(include=numerics)
-# 
-# for col in newData:
-#     mean = newData[col].mean()
-#     std = newData[col].std()
-#     z = ((newData[col]-mean)/std)
+# for epoch in range(epochs):
+#     inputs = trainData
+#     
+#     for l in range(layers):
+#         XW = np.dot(trainData, weights) + bias
+#     print(XW)
+# =============================================================================
+    
 # =============================================================================
